a843aff9a24223dd29d9cff08656755b18a8dd59
durai-babu-rajendhiran <durairaja00720@gmail.com>
2025-04-13 15:53:25 +0530

    Second as Half Price

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
@@ -9,70 +11,73 @@ class BasketController extends Controller
 
 
 
-    // working on second half price
-//     public function index()
-// {
-//     $products = Product::all();
-//     $basket = Session::get('basket', []);
-
-//     $subtotal = 0;
-//     $discount = 0;
-
-//     foreach ($basket as $sku => $item) {
-//         $quantity = $item['quantity'];
-//         $price = $item['price'];
-
-//         // Standard total for this product
-//         $itemTotal = $price * $quantity;
-//         $subtotal += $itemTotal;
-
-//         // Special offer: every second item is half price
-//         $halfPriceCount = floor($quantity / 2);
-//         $discount += ($price / 2) * $halfPriceCount;
-//     }
-
-//     $subtotalAfterDiscount = $subtotal - $discount;
-
-//     // Delivery charge rules
-//     if ($subtotalAfterDiscount >= 90) {
-//         $delivery = 0;
-//     } elseif ($subtotalAfterDiscount >= 50) {
-//         $delivery = 2.95;
-//     } else {
-//         $delivery = 4.95;
-//     }
-
-//     $total = $subtotalAfterDiscount + $delivery;
-
-//     return view('basket.index', compact(
-//         'products', 'basket', 'subtotal', 'discount', 'delivery', 'total'
-//     ));
-// }
-
+    // second one half price
     public function index()
     {
         $products = Product::all();
         $basket = Session::get('basket', []);
-    
+
         $subtotal = 0;
-    
-        foreach ($basket as $item) {
-            $subtotal += $item['price'] * $item['quantity'];
+        $discount = 0;
+
+        foreach ($basket as $sku => $item) {
+            $quantity = $item['quantity'];
+            $price = $item['price'];
+
+            // Standard total for this product
+            $itemTotal = $price * $quantity;
+            $subtotal += $itemTotal;
+
+            // Special offer: every second item is half price
+            $halfPriceCount = floor($quantity / 2);
+            $discount += ($price / 2) * $halfPriceCount;
         }
-    
-        // Delivery charge logic
-        if ($subtotal >= 90) {
+
+        $subtotalAfterDiscount = $subtotal - $discount;
+
+        // Delivery charge rules
+        if ($subtotalAfterDiscount >= 90) {
             $delivery = 0;
-        } elseif ($subtotal >= 50) {
+        } elseif ($subtotalAfterDiscount >= 50) {
             $delivery = 2.95;
         } else {
             $delivery = 4.95;
         }
-    
-        $total = $subtotal + $delivery;
-    
-        return view('basket.index', compact('products', 'basket', 'subtotal', 'delivery', 'total'));
+
+        $total = $subtotalAfterDiscount + $delivery;
+
+        return view('basket.index', compact(
+            'products',
+            'basket',
+            'subtotal',
+            'discount',
+            'delivery',
+            'total'
+        ));
     }
+
+    // public function index()
+    // {
+    //     $products = Product::all();
+    //     $basket = Session::get('basket', []);
+
+    //     $subtotal = 0;
+
+    //     foreach ($basket as $item) {
+    //         $subtotal += $item['price'] * $item['quantity'];
+    //     }
+    //     if ($subtotal >= 90) {
+    //         $delivery = 0;
+    //     } elseif ($subtotal >= 50) {
+    //         $delivery = 2.95;
+    //     } else {
+    //         $delivery = 4.95;
+    //     }
+
+    //     $total = $subtotal + $delivery;
+
+    //     return view('basket.index', compact('products', 'basket', 'subtotal', 'delivery', 'total'));
+    // }
 
     public function add(Request $request)
     {
